b60aa55830c76bf1e582524091285a16a187689d
xavl <xavl-doali@gmail.com>
2021-12-14 14:53:45 +0100

    From scratch

--- compile_snake.sh
+++
@@ -1,4 +0,0 @@
-# https://www.sfml-dev.org/tutorials/2.4/start-linux-fr.php
-#sudo apt install libsfml-dev libsfml-doc
-
-g++ -o test-sfml src/test-sfml.cpp -lsfml-graphics -lsfml-window -lsfml-system
