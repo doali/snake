dd26c266ab436049eab60e5801d24ed59e9ac1f0
doali <doali@gmail.com>
2022-12-11 23:30:49 +0100

    Timer experiments...

---
+++ headers/Timer.hpp
@@ -0,0 +1,54 @@
+#ifndef _TIMER_HPP
+#define _TIMER_HPP
+
+#include <SFML/Graphics.hpp>
+#include <functional>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+
+namespace timer
+{
+    static void info_time_sec()
+    {
+        std::time_t result = std::time(nullptr);
+        std::cout << std::asctime(std::localtime(&result))
+                  << result << " seconds since the Epoch\n";
+    }
+
+    struct Timer
+    {
+        explicit Timer(sf::RenderWindow &p_window, unsigned int p_milli) : _window(p_window), _milli(p_milli) {}
+        ~Timer() = default;
+
+        void add(std::function<void(sf::RenderWindow &)> p_callback, sf::RenderWindow &p_window)
+        {
+            _callback = p_callback;
+        }
+
+        void start()
+        {
+            _start = std::chrono::high_resolution_clock::now();
+        }
+
+        void run()
+        {
+            auto l_now{std::chrono::high_resolution_clock::now()};
+            auto elapsed{std::chrono::duration_cast<std::chrono::milliseconds>(l_now - _start)};
+
+            if (elapsed.count() >= _milli)
+            {
+                _callback(_window);
+                _start = l_now;
+                info_time_sec();
+            }
+        }
+
+        sf::RenderWindow &_window;
+        unsigned int _milli{0};
+        std::function<void(sf::RenderWindow &)> _callback;
+        std::chrono::high_resolution_clock::time_point _start;
+    }; // namespace timer
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
 #include "Snake.hpp"
+#include "Timer.hpp"
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+
+#include <functional>
+
+static sf::CircleShape shape(10.f);
+
+void update_cirle(sf::RenderWindow &window)
+{        
+    static unsigned int val{0U};
+    static unsigned int l_val{0U};
+    l_val = (val < (2 * M_PI * 1000)) ? ++val : 0U;
+
+    shape.setRadius(10);//cos(l_val / (2 * M_PI)) * 5 + 5);
+    shape.setPosition(100 + 50 * cos(l_val / (2 * M_PI)), 100 + 50 * sin(l_val / (2 * M_PI)));
+    shape.setFillColor(sf::Color::Magenta);    
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
 
-    snake::Snake snake;    
+    snake::Snake snake;
     snake.init();
+
+    timer::Timer l_timer{window, 60};    
+    l_timer.add(update_cirle, window);
+    l_timer.start();
 
     while (window.isOpen())
     {
@@ -23,16 +48,16 @@ int main()
                 {
                 case sf::Keyboard::Right:
                     snake.move(snake::Direction::RIGHT);
-                    break;                
+                    break;
                 case sf::Keyboard::Down:
                     snake.move(snake::Direction::DOWN);
-                    break;                
+                    break;
                 case sf::Keyboard::Left:
                     snake.move(snake::Direction::LEFT);
-                    break;                
+                    break;
                 case sf::Keyboard::Up:
                     snake.move(snake::Direction::UP);
-                    break;                
+                    break;
                 default:
                     break;
                 }
@@ -40,12 +65,15 @@ int main()
         }
 
         window.clear();
-
+        
         for(auto &cell : snake._snakeCells)
         {
             window.draw(cell._shape);
         }
-        
+            
+        l_timer.run();
+        window.draw(shape);
+
         window.display();
     }
 
